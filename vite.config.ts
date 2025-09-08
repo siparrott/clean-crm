f335ad09fae5eb884e485df87b5b5f92a21c3ea9
Simon Parrott <hallo@newagefotografie.com>
2025-09-08 18:17:40 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:10000',
+        target: 'http://localhost:3000',
         changeOrigin: true,
       },
     },
